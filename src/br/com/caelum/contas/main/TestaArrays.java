dd3ab29e75a18c7f79041625850499a682bb8483
Mariana <marimavelar@gmail.com>
2020-01-30 22:22:50 -0200

    Aula 09 - Arrays

---
+++ src/br/com/caelum/contas/main/TestaArrays.java
@@ -0,0 +1,26 @@
+package br.com.caelum.contas.main;
+
+import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.ContaCorrente;
+
+public class TestaArrays {
+	public static void main(String[] args) {
+		Conta[] contas = new Conta[10];
+		
+		for(int i = 0; i < contas.length; i++) {
+			Conta conta = new ContaCorrente();
+			conta.deposita(i * 100.0);
+			contas[i] = conta;
+			System.out.println(contas[i].getSaldo());
+		}
+		
+		double mediaSaldos = 0;
+		for(int i = 0; i < contas.length; i++) {
+			mediaSaldos += contas[i].getSaldo();
+		}
+		mediaSaldos = mediaSaldos / contas.length;
+		System.out.println(mediaSaldos);
+		
+		
+	}
+}
